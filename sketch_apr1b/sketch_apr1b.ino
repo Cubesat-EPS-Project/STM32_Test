5804e32344a9c2a61cda45b104b9da2d5386bfd0
Naveen A.B <53806497+RNA3210d@users.noreply.github.com>
2023-04-08 17:52:37 +0530

    Update sketch_apr1b.ino

--- sketch_apr1b/sketch_apr1b.ino
+++ sketch_apr1b/sketch_apr1b.ino
@@ -2,7 +2,7 @@
 float multiplier =0.1; 
 float R1=30000.0;
 float R2=7440.0;
-float ref=4.12;
+float ref=4.09;
 
 void setup() {  
 
@@ -21,13 +21,13 @@ void loop() {
   for(int i=0;i<200;i++)
   {
   float SensorRead = analogRead(A0)*(5.0 / 1023.0);     //We read the sensor output  
-  float Current = (SensorRead-2.49)/multiplier;                  //Calculate the current value508
+  float Current = (SensorRead-2.44)/multiplier;                  //Calculate the current value508
   float SensorRead2 = analogRead(A1)*(5.0 / 1023.0);     //We read the sensor output  
-  float Current2 = (SensorRead2-2.5)/multiplier;                  //Calculate the current value
+  float Current2 = (SensorRead2-2.49)/multiplier;                  //Calculate the current value
   float adc=analogRead(A2);
   //Serial.println(adc);
   float volt= ((adc*ref)/1024)/(R2/(R1+R2));
-  volt=volt;
+  //volt=adc;
   sum1=Current + sum1;
   sum2=Current2 + sum2;
   sum3=volt+sum3;
